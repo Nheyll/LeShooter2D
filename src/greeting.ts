895dce3e26733917ac1fed8feca997b009f8b4db
Nheyll <nheyll.dev@gmail.com>
2023-03-03 18:02:35 +0100

    Minimal TS template

---
+++ src/greeting.ts
@@ -0,0 +1,4 @@
+export function greet(name: string) {
+    console.log(`Hellooo, ${name}!`);
+}
+  
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { greet } from "./greeting";
+
+const name: string = "TypeScript";
+greet(name);
